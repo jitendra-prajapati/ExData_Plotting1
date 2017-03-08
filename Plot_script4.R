265614155edebca556e4637d54f7d35154558e17
jitendra-prajapati <jitendrappr990@gmail.com>
2017-03-08 16:59:03 +0530

    Update Plot_script4.R

--- Plot_script4.R
+++ Plot_script4.R
@@ -7,7 +7,7 @@
 	df=read.table("./household_power_consumption.txt",header=T, sep=";")
 
 ## convert the date string to Date structure in format %d/%m/%Y
-	df$Date <- strptime(df$Date, format="%Y-%m-%d")
+	df$Date <- strptime(df$Date, format="%d/%m/%Y")
 
 ## subset the data base on required date  
 	data <- df[(df$Date=="2007-02-01") | (df$Date=="2007-02-02"),]
